f3bf7334cfcf42b44e333d718a2cb45de3670654
Samual Norman <me@samual.uk>
2023-12-19 16:27:58 +0000

    add scripts can now make a package in `dist`

---
+++ scripts/package.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -ex
+rm -rf dist
+scripts/build.sh
+scripts/emit-declarations.sh
+scripts/emit-package-json.js
+cp LICENSE README.md dist
---
+++ scripts/update.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+set -ex
+pnpm update --latest
